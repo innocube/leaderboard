f3d2b3bc1c8fa1f777900800cfc0349d6f71091b
Paul Chung <paul.chung@innocube.net>
2015-02-24 15:24:55 +0900

    Session Added
    - selected item is stored & retrieved in a session

--- leaderboard2.js
+++ leaderboard2.js
@@ -1,28 +1,35 @@
 PlayersList = new Mongo.Collection('players');
 
-if (Meteor.isClient) {
-    // this code only runs on the client:
+
+if (Meteor.isClient) {                      // this code only runs on the client:
     console.log("Hello client");
 
-    Template.leaderboard.helpers({
-        // helper functions go here:
+    Template.leaderboard.helpers({          // helper functions go here:
         'player': function(){
             return PlayersList.find()
+        },
+        'selectedClass': function(){
+            var playerId = this._id;
+            var selectedPlayer = Session.get('selectedPlayer');
+            if (playerId == selectedPlayer) {
+                console.log("Selected Player: " + selectedPlayer);
+                return "selected"
+            }
         },
         'count': function(){
             return PlayersList.find().count();
         }
     });
 
-    Template.leaderboard.events({
-       // events go here:
+    Template.leaderboard.events({           // events go here:
         'click .player': function(){
-            console.log("You clicked a .player element");
+            var playerId = this._id;
+            Session.set('selectedPlayer', playerId);
         }
     });
 }
 
-if (Meteor.isServer) {
-    // this code only runs on the server:
+
+if (Meteor.isServer) {                      // this code only runs on the server:
     console.log("Hello server");
 }
